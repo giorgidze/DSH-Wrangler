eea2de2ab85bfdc124fa19f00e07db900f9d3a3e
Manuel Dietrich <manuel@eisbaer.(none)>
2012-02-27 15:58:36 +0100

    bug fix for dsh_groupWith() --> function works

--- js/list.func.js
+++ js/list.func.js
@@ -178,8 +178,8 @@ function dsh_filter(f, as) {
   return bs;  
 }
 
-// TODO
-/*function dsh_groupWith(f, as) {
+// GROUPWITH 
+function dsh_groupWith(f, as) {
   var l = as.value.length;
   var groups = new Object();
 
@@ -187,7 +187,7 @@ function dsh_filter(f, as) {
   // group the values in these keys (arrays)
   for (i = 0; i < l; i++) {
     var p = f(as.value[i]);
-    if(!eval('groups.'+p.value)) {
+    if(eval('groups.'+p.value+" == null")) {
       eval('groups.'+p.value+' = new Array()');
       eval('groups.'+p.value+'.push(as.value[i])');
     } else {
@@ -196,16 +196,17 @@ function dsh_filter(f, as) {
   }
 
   var bs = dsh_empty();                     // new list (contains the groups later)
-  for (j=0; j < groups.length; j++) {
-    var cs = dsh_empty();                   // create a new list for every group
-    for(k=0; k<groups[j]; k++) {
-      cs = dsh_snoc(cs,groups[j][k]));      // fill group list with elements
+
+  for (result in groups) {					// for every group ...
+    var cs = dsh_empty();                   // create a new list ...
+    for(k=0; k<groups[result].length; k++) {
+      cs = dsh_snoc(cs,groups[result][k]);  // and fill with its elements
     }
     bs = dsh_snoc(bs, cs);                  // add group to main list
   }
 
   return bs; 
-}*/
+}
 
 
 function dsh_sortWith(f, as) {
