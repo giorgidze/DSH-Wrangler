93cdd1c72529d392a2234da8c7e05aca9208f8c0
Manuel Dietrich <manuel@eisbaer.(none)>
2012-02-28 10:22:13 +0100

    bug fix for dsh_lt, dsh_gt, dsh_lte; dsh_gte

--- js/equality.func.js
+++ js/equality.func.js
@@ -14,16 +14,32 @@ function dsh_neq(a,b) {
 // LT: is a lesser than b?
 // (Ord a, QA a) => Q a -> Q a -> Q Bool
 function dsh_lt(a, b) {
-  if(a.type == b.type) {
+  if(JSON.stringify(a.type) == JSON.stringify(b.type)) {
     var type_constructor = a.type.type_constructor;
     if((type_constructor == "Integer") || (type_constructor == "Double")) {
       return dsh_bool((a.value < b.value));
-    } else if(type_constructor == "Char") {
-      return dsh_bool((a.value.charCodeAt(0) < b.value.charCodeAt(0)));
-    } else if((type_constructor == "Text") || (type_constructor == "List")) {
-      return dsh_bool((a.value.length < b.value.length));
-    } else if(type_constructor.substr(0,5) != "Tuple") {
-        // TODO
+    } else if((type_constructor == "Char") || (type_constructor == "Text")) {
+      if(a.value.length == b.value.length) {
+        for(var i=0; i<a.value.length; i++) {
+          if(a.value.charCodeAt(i) != b.value.charCodeAt(i)) {
+            return dsh_bool((a.value.charCodeAt(i) < b.value.charCodeAt(i)));
+          }
+        }
+        return dsh_bool(false);
+      } else {
+        return dsh_bool((a.value.length < b.value.length));
+      }
+    } else if((type_constructor == "Tuple") || (type_constructor == "List")) {
+      if(a.value.length == b.value.length) {
+        for(var i=0; i<a.value.length; i++) {
+          if(dsh_cond(dsh_lt(a.value[i], b.value[i]),false,true)) {
+            return dsh_bool(true);
+          }
+        }
+        return dsh_bool(false);
+      } else {
+        return dsh_bool((a.value.length < b.value.length));
+      }
     } else {
       throw new Error("Input is not a valid type.");
     }
@@ -36,16 +52,32 @@ function dsh_lt(a, b) {
 // GT: is a greater than b?
 // (Ord a, QA a) => Q a -> Q a -> Q Bool
 function dsh_gt(a, b) {
-  if(a.type == b.type) {
+  if(JSON.stringify(a.type) == JSON.stringify(b.type)) {
     var type_constructor = a.type.type_constructor;
     if((type_constructor == "Integer") || (type_constructor == "Double")) {
       return dsh_bool((a.value > b.value));
-    } else if(type_constructor == "Char") {
-      return dsh_bool((a.value.charCodeAt(0) > b.value.charCodeAt(0)));
-    } else if((type_constructor == "Text") || (type_constructor == "List")) {
-      return dsh_bool((a.value.length > b.value.length));
-    } else if(type_constructor.substr(0,5) != "Tuple") {
-        // TODO
+    } else if((type_constructor == "Char") || (type_constructor == "Text")) {
+      if(a.value.length == b.value.length) {
+        for(var i=0; i<a.value.length; i++) {
+          if(a.value.charCodeAt(i) != b.value.charCodeAt(i)) {
+            return dsh_bool((a.value.charCodeAt(i) > b.value.charCodeAt(i)));
+          }
+        }
+        return dsh_bool(false);
+      } else {
+        return dsh_bool((a.value.length > b.value.length));
+      }
+    } else if((type_constructor == "Tuple") || (type_constructor == "List")) {
+      if(a.value.length == b.value.length) {
+        for(var i=0; i<a.value.length; i++) {
+          if(dsh_cond(dsh_gt(a.value[i], b.value[i]),false,true)) {
+            return dsh_bool(true);
+          }
+        }
+        return dsh_bool(false);
+      } else {
+        return dsh_bool((a.value.length > b.value.length));
+      }
     } else {
       throw new Error("Input is not a valid type.");
     }
@@ -58,16 +90,32 @@ function dsh_gt(a, b) {
 // LTE: is a lesser than b or equal?
 // (Ord a, QA a) => Q a -> Q a -> Q Bool
 function dsh_lte(a, b) {
-  if(a.type == b.type) {
+  if(JSON.stringify(a.type) == JSON.stringify(b.type)) {
     var type_constructor = a.type.type_constructor;
     if((type_constructor == "Integer") || (type_constructor == "Double")) {
       return dsh_bool((a.value <= b.value));
-    } else if(type_constructor == "Char") {
-      return dsh_bool((a.value.charCodeAt(0) <= b.value.charCodeAt(0)));
-    } else if((type_constructor == "Text") || (type_constructor == "List")) {
-      return dsh_bool((a.value.length <= b.value.length));
-    } else if(type_constructor.substr(0,5) != "Tuple") {
-        // TODO
+    } else if((type_constructor == "Char") || (type_constructor == "Text")) {
+      if(a.value.length == b.value.length) {
+        for(var i=0; i<a.value.length; i++) {
+          if(a.value.charCodeAt(i) != b.value.charCodeAt(i)) {
+            return dsh_bool((a.value.charCodeAt(i) <= b.value.charCodeAt(i)));
+          }
+        }
+        return dsh_bool(true);
+      } else {
+        return dsh_bool((a.value.length <= b.value.length));
+      }
+    } else if((type_constructor == "Tuple") || (type_constructor == "List")) {
+      if(a.value.length == b.value.length) {
+        for(var i=0; i<a.value.length; i++) {
+          if(dsh_cond(dsh_gt(a.value[i], b.value[i]),false,true)) {
+            return dsh_bool(false);
+          }
+        }
+        return dsh_bool(true);
+      } else {
+        return dsh_bool((a.value.length <= b.value.length));
+      }
     } else {
       throw new Error("Input is not a valid type.");
     }
@@ -80,16 +128,32 @@ function dsh_lte(a, b) {
 // GTE: is a greater than b or equal?
 // (Ord a, QA a) => Q a -> Q a -> Q Bool
 function dsh_gte(a, b) {
-  if(a.type == b.type) {
+  if(JSON.stringify(a.type) == JSON.stringify(b.type)) {
     var type_constructor = a.type.type_constructor;
     if((type_constructor == "Integer") || (type_constructor == "Double")) {
       return dsh_bool((a.value >= b.value));
-    } else if(type_constructor == "Char") {
-      return dsh_bool((a.value.charCodeAt(0) >= b.value.charCodeAt(0)));
-    } else if((type_constructor == "Text") || (type_constructor == "List")) {
-      return dsh_bool((a.value.length >= b.value.length));
-    } else if(type_constructor.substr(0,5) != "Tuple") {
-        // TODO
+    } else if((type_constructor == "Char") || (type_constructor == "Text")) {
+      if(a.value.length == b.value.length) {
+        for(var i=0; i<a.value.length; i++) {
+          if(a.value.charCodeAt(i) != b.value.charCodeAt(i)) {
+            return dsh_bool((a.value.charCodeAt(i) >= b.value.charCodeAt(i)));
+          }
+        }
+        return dsh_bool(true);
+      } else {
+        return dsh_bool((a.value.length >= b.value.length));
+      }
+    } else if((type_constructor == "Tuple") || (type_constructor == "List")) {
+      if(a.value.length == b.value.length) {
+        for(var i=0; i<a.value.length; i++) {
+          if(dsh_cond(dsh_lt(a.value[i], b.value[i]),false,true)) {
+            return dsh_bool(false);
+          }
+        }
+        return dsh_bool(true);
+      } else {
+        return dsh_bool((a.value.length >= b.value.length));
+      }
     } else {
       throw new Error("Input is not a valid type.");
     }
@@ -99,7 +163,7 @@ function dsh_gte(a, b) {
 }
 
 
-// MINIMUM --> TODO: Tuples?!
+// MINIMUM
 function dhc_min(as) {
   if (as.value != null) {
     var min_value = as.value[0];
@@ -116,7 +180,7 @@ function dhc_min(as) {
 }
 
 
-// MAXIMUM --> TODO: Tuples?!
+// MAXIMUM
 function dhc_max(as) {
   if (as.value != null) {
     var max_value = as.value[0];
